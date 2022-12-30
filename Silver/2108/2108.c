80e8910c2d613be1f12c63374f5661fdacb0c7e1
hyun <hyun22@mail.hongik.ac.kr>
2022-12-30 17:50:48 +0900

    DEC 30 2022

--- Silver/2108/2108.c
+++ Silver/2108/2108.c
@@ -1,168 +1,96 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-typedef struct _number
-{
-	int value;
-	int appear;
-	struct _number *next;
-}number;
+int arr[500000];
+int cnt[8001];
+int N;
 
-
-void add_number(number **head, int num);
-number *new_nubmer(int num);
-number *is_appeared(number *head, int num);
-
-void add_number(number **head, int num)
-{
-	number *prev = NULL;
-	number *cur = NULL;
-	if (!*head)
-	{
-		*head = new_nubmer(num);
-		return ;
-	}
-	if ((cur = is_appeared(*head, num)))
-	{
-		cur->appear++;
-		return ;
-	}
-	number *new = new_nubmer(num);
-	for (cur = *head; cur; )
-	{
-		prev = cur;
-		cur = cur->next;
-		if (num < cur->value)
-		{
-			break;
-		}
-	}
-	if (prev == cur)
-	{
-		if (prev == *head)
-			*head = new;
-		
-	}
-	else
-		prev->next = new;
-	new->next = cur;
-}
-
-number *new_nubmer(int num)
-{
-	number *new = (number *)malloc(sizeof(number));
-	new->value = num;
-	new->appear = 1;
-	new->next = NULL;
-	return new;
-}
-
-number *is_appeared(number *head, int num)
-{
-	number *cur = NULL;
-
-	for (cur = head; cur; cur = cur->next)
-	{
-		if (cur->value == num)
-			return cur;
-	}
-	return NULL;
-}
-
-void free_lst(number **head)
-{
-	number *cur;
-	number *next;
-
-	for (cur = *head; cur; cur = next)
-	{
-		next = cur->next;
-		free(cur);
-	}
-	*head = NULL;
-}
-/*
-number *search(number *head, int num)
-{
-	number *cur;
-
-	for (cur = head; cur; cur = cur->next)
-	{
-		if (cur->value == num)
-			return (cur);
-	}
-	return (NULL);
-}
-*/
-/*
 int compare(const void *first, const void *second)
 {
-	int *a = (int *)first;
-	int *b = (int *)second;
+	int a;
+	int b;
 
-	if (*a > *b)
-		return 1;
-	else if (*a == *b)
-		return 0;
-	return -1;
+	a = *(int *)first;
+	b = *(int *)second;
+	if (a > b)
+		return (1);
+	else if (a == b)
+		return (0);
+	return (-1);
 }
-
-
 
 int get_mean(int *arr, int size)
 {
+	double res;
+
+	res = 0.0;
+	for (int i = 0; i < size; ++i)
+		res += arr[i];
+	res /= size;
+	return ((res == 0.0 || res > 0.0) ? (int)(res + 0.5) : (int)(res - 0.5));
 }
 
 int get_median(int *arr, int size)
 {
+	if (size % 2 == 0)
+		return ((arr[size / 2] + arr[size / 2 - 1]) / 2);
+	return (arr[size / 2]);
 }
 
-int get_mode(int *arr, int size)
+int get_mode(void)
 {
-	
+	int mode;
+	int max_seq;
+	int j;
+
+	max_seq = 1;
+	for (int i = 0; i - 4000 <= arr[N - 1]; ++i)
+	{
+		if (cnt[i] == max_seq || cnt[i] > max_seq)
+		{
+			max_seq = cnt[i];
+		}
+	}
+
+	j = 0;
+	for (int i = 0; i - 4000 <= arr[N - 1]; ++i)
+	{
+		if (cnt[i] ==  max_seq)
+		{
+			mode = i;
+			j++;
+			if (j == 2)
+				break;
+		}
+	}
+
+	return (mode - 4000);
 }
 
 int get_range(int *arr, int size)
 {
-
+	return (arr[size - 1] - arr[0]);
 }
-*/
 
 int main(void)
 {
-	/*
-	int N;
-	int arithmetic_mean;
+	int mean;
 	int median;
 	int mode;
 	int range;
-	int tmp;
 
-	scanf("%d", N);
-	for (int i = 0; i < N; ++i)
-	{
-		
-	}
-	*/
-
-	int N;
-	int tmp;
-	number *head;
 	scanf("%d", &N);
 	for (int i = 0; i < N; ++i)
 	{
-		scanf("%d", &tmp);
-		add_number(&head, tmp);
+		scanf("%d", &arr[i]);
+		cnt[arr[i] + 4000]++;
 	}
-
-	number *cur;
-	for (cur = head; cur; cur = cur->next)
-	{
-		printf("val: %d apperances: %d\n", cur->value, cur->appear);
-	}
-
-	free_lst(&head);
-
-	return 0;
+		
+	qsort(arr, N, sizeof(int), compare);
+	mean = get_mean(arr, N);
+	median = get_median(arr, N);
+	mode = get_mode();
+	range = get_range(arr, N);
+	printf("%d\n%d\n%d\n%d", mean, median, mode, range);
+	return (0);
 }
