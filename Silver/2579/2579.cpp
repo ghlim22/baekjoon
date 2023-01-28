170be16c87b0d877a37c629888a942c4dc6fbf3a
ghlim22 <hyun22@mail.hongik.ac.kr>
2023-01-28 21:26:57 +0900

    solved 2579

--- Silver/2579/2579.cpp
+++ Silver/2579/2579.cpp
@@ -8,11 +8,7 @@
 using namespace std;
 
 static int stairs[300];
-static int dp[300];
-
-void solve() {
-	
-}
+static int res[300];
 
 signed main() {
 	fastio;
@@ -23,8 +19,15 @@ signed main() {
 	cin >> stairs_cnt;
 	for (int i = 0; i < stairs_cnt; ++i)
 		cin >> stairs[i];
-	dp[0] = stairs[0];
-	dp[1] = dp[0] + stairs[1];
-	dp[2] = dp[0] + stairs[2];	
+	
+	res[0] = stairs[0];
+	res[1] = stairs[0] + stairs[1];
+	res[2] = max(stairs[0], stairs[1]) + stairs[2];
 
+	for (int i = 3; i < stairs_cnt; ++i)
+		res[i] = max(res[i - 2] + stairs[i], res[i - 3] + stairs[i - 1] + stairs[i]);
+
+	cout << res[stairs_cnt - 1];
+
+	return (0);
 }
