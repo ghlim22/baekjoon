3155b9058db47983cb9e3a490bdab02986ec4bbd
ghlim22 <hyun22@mail.hongik.ac.kr>
2023-02-12 13:57:24 +0900

    edited 1477

--- Gold/1477/1477.cpp
+++ Gold/1477/1477.cpp
@@ -4,17 +4,8 @@
 #include <iostream>
 #include <algorithm>
 #define fastio cin.tie(0)->sync_with_stdio(0)
-#define precision(x) cout << fixed; cout.precision(x)
-#define ll long long
-#define pii pair<int, int>
 
 using namespace std;
-
-
-
-void solve() {
-	
-}
 
 signed main() {
 	fastio;
