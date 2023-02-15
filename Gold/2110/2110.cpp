96d1ae0b90c93d4ab84125241d32ec0b0af98c98
hyun <hyun22@mail.hongik.ac.kr>
2023-02-15 19:46:17 +0900

    2110 solved

--- Gold/2110/2110.cpp
+++ Gold/2110/2110.cpp
@@ -1,67 +1,47 @@
 #include <iostream>
 #include <algorithm>
-
 #define fastio cin.tie(0)->sync_with_stdio(0)
-#define precision(x) \
-    cout << fixed;   \
-    cout.precision(x)
-#define ll long long
-#define pii pair<int, int>
 
 using namespace std;
 
-int arr[200000];
-int N, C;
-
-void solve()
-{
-}
+int houses[200000];
 
 signed main()
 {
     fastio;
 
-    // 집의 개수와 공유기 개수를 입력받고 좌표 입력받은 후 오름차순 정렬.
-    cin >> N >> C;
-    for (int i = 0; i < N; ++i)
-    {
-        cin >> arr[i];
-    }
-    sort(arr, arr + N);
+    int house_cnt = 0;
+    int router_cnt = 0;
 
-    // 이진 탐색 활용.
-    int left = 1;               // 최소 거리.
-    int right = arr[N - 1] - 1; // 최대 거리.
-    int cnt;                    // 설치한 공유기의 수.
-    int mid; // 중간 거리.
-    int cur; // 공유기를 설치한 위치.
-    int ans = 0; // 답.
-
+    cin >> house_cnt >> router_cnt;
+    for (int i = 0; i < house_cnt; ++i)
+        cin >> houses[i];
+    sort(houses, houses + house_cnt);
+    int left = 1;
+    int right = houses[house_cnt - 1] - houses[0];
+    int ans = 0;
     while (left <= right)
     {
-        cnt = 1; // 공유기 1개 설치.
-        mid = (left + right) / 2; // 중간 거리 구하기.
-        cur = 0; // 공유기를 설치한 기준 좌표의 인덱스.
-        for (int i = 1; i < N; ++i)
+        int mid = left + (right - left) / 2;
+        int cur = 0;
+        int cnt = 1;
+        for (int i = 1; i < house_cnt; ++i)
         {
-            if (arr[i] - arr[cur] >= mid) // 기준 좌표와 비교 좌표의 거리 차가 mid 이상이면 공유기를 설치한다. 기준 좌표를 최신화한다.
+            if (houses[i] - houses[cur] >= mid)
             {
-                cnt++;
                 cur = i;
+                cnt++;
             }
+            if (cnt >= router_cnt)
+                break ;
         }
-        if (cnt >= C) // 공유기를 c대 이상 설치했다면...
-        {
-            ans = mid; // 중간 거리를 정답에 저장한다.
-            left = mid + 1; // 더 큰 거리를 탐색한다.
-        }
+        if (cnt >= router_cnt)
+            left = mid + 1;
         else
-        {
-            right = mid - 1; // 가능한 거리의 크기를 좁힌다.
-        }
+            right = mid - 1;
     }
-
-    cout << ans;
+    
+    cout << right;
 
     return 0;
 }
