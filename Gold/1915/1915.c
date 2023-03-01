784cd275475fc079ed25bda43c673e3578c8621f
hyun <hyun22@mail.hongik.ac.kr>
2023-03-01 11:24:51 +0900

    1915 solved

--- Gold/1915/1915.c
+++ Gold/1915/1915.c
@@ -1,62 +1,43 @@
 #include <stdio.h>
-#include <stdbool.h>
 #define MAX_SIZE (1000)
 
-static int board[MAX_SIZE][MAX_SIZE];
-static int dp[MAX_SIZE][MAX_SIZE];
-static int n, m;
+static int	matrix[MAX_SIZE][MAX_SIZE];
+static int	dp[MAX_SIZE][MAX_SIZE];
+static int	maximum_size;
 
-static bool check(int x, int y)
+static int get_min(int x, int y)
 {
-	if (x - 1 < 0 || y - 1 < 0)
-		return (false);
-	return (board[y - 1][x] && board[y][x - 1] && board[y - 1][x - 1]);
-}
-
-static int get_max(int a, int b, int c)
-{
-	int max = a;
-
-	if (b > max)
-		max = b;
-	return ((max > c) ? max : c);
+	return (x < y ? x : y);
 }
 
 int main(void)
 {
+	int n, m;
+
 	scanf("%d %d", &n, &m);
 	for (int i = 0; i < n; ++i)
 	{
 		for (int j = 0; j < m; ++j)
-			scanf("%1d", &board[i][j]);
+			scanf("%1d", &matrix[i][j]);
 	}
 
 	for (int i = 0; i < n; ++i)
 	{
 		for (int j = 0; j < m; ++j)
 		{
-			int cnt;
-
-			if (i - 1 < 0 || j - 1 < 0)
-				cnt = board[i][j];
+			if (i == 0 || j == 0)
+				dp[i][j] = matrix[i][j];
+			else if (matrix[i][j] == 1)
+				dp[i][j] = get_min(get_min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
 			else
-				cnt = get_max(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]);
-
-			if (board[i][j] && i == j && check(j, i))
-				dp[i][j] = cnt + 1;
-			else
-				dp[i][j] = cnt;
+				dp[i][j] = 0;
+			
+			if (dp[i][j] > maximum_size)
+				maximum_size = dp[i][j];
 		}
 	}
 
-	printf("%d\n", dp[n - 1][m - 1]);
-
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = 0; j < m; ++j)
-			printf("%d", dp[i][j]);
-		printf("\n");
-	}
+	printf("%d", maximum_size * maximum_size);
 
 	return (0);
 }
