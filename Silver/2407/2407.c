e980ea6c6fe84b92a456196c9d15ad9475f45ee3
hyun <hyun22@mail.hongik.ac.kr>
2023-03-19 19:29:13 +0900

    2407 solved

--- Silver/2407/2407.c
+++ Silver/2407/2407.c
@@ -1,142 +1,56 @@
 #include <stdio.h>
-#include <stdlib.h>
-#define MAX (1000)
-static char	arr1[1000];
-static char arr2[1000];
-
-static char *add(char *a, char *b)
-{	
-	int	start_a = 0;
-	int start_b = 0;
-	int carry;
-	int i;
-
-	char *res = (char *)calloc(MAX, sizeof(char));
-	while (start_a > MAX - 1 && a[start_a] == 0)
-		start_a++;
-	while (start_b > MAX - 1 && b[start_b] == 0)
-		start_b++;
-	int max_start = start_a <= start_b ? start_a : start_b;
-	i = MAX - 1;
-	carry = 0;
-	while (i >= max_start)
-	{
-		int sum = a[i] + b[i];
-		if (sum >= 10)
-		{
-			res[i] = carry + (sum % 10);
-			carry = 1;
-		}
-		else
-		{
-			res[i] = carry + sum;
-			carry = 0;
-		}
-		i--;
-	}
-	if (carry)
-		res[i] = carry;
-	return (res);
-}
-
-static void print_arr(char *arr)
-{
-	int start_idx = 0;
-
-	while (start_idx < MAX - 1 && arr[start_idx] == 0)	
-		start_idx++;
-	for (int i = start_idx; i < MAX; ++i)
-		printf("%d", arr[i]);
-}
-
-static int	is_zero(char *arr)
-{
-	int idx = 0;
-	while (idx < MAX && arr[idx] == 0)
-		idx++;
-	return (idx == MAX);
-}
-
-static int is_same_num(char *a, char *b)
-{
-	int a_idx = 0;
-	int b_idx = 0;
-
-	while (a_idx < MAX - 1 && a[a_idx] == 0)
-		a_idx++;
-	while (b_idx < MAX - 1 && b[b_idx] == 0)
-		b_idx++;
-	if (a_idx != b_idx)
-		return (0);
-	for (int i = a_idx; i < MAX; ++i)
-	{
-		if (a[i] != b[i])
-			return (0);
-	}
-	return (1);
-}
-
-static char *number_one(void)
-{
-	char *res = (char *)calloc(MAX, sizeof(char));
-	res[MAX - 1] = 1;
-	return (res);
-}
-
-static char *minus_one(char *a)
-{
-	int start_idx = 0;
-	int carry = 0;
-
-	if (a[MAX - 1] > 0)
-	{
-		a[MAX - 1]--;
-		return (a);
-	}
-	while (start_idx < MAX - 1 && a[start_idx])
-		start_idx++;
-	for (int i = MAX - 1; i >= start_idx; --i)
-	{
-		if (a[i] - 1 < 0)
-		{
-			a[i] = 0;
-			carry = -1;
-		}
-		else
-		{
-			a[i] = a[i] - 1 + carry;
-			carry = 0;
-		}
-	}
-	return (a);
-}
-
-static char *comb(char *a, char *b)
-{
-	if (is_same_num(a, b) || is_zero(b))
-		return (number_one());
-	return (add(comb(minus_one(a), minus_one(b)), comb(minus_one(a), b))); 
-}
-
-static void put_num(char *arr, int num)
-{
-	for (int i = MAX - 1; i > 0; --i)
-	{
-		arr[i] = num % 10;
-		num /= 10;
-		if (!num)
-			break;
-	}
-}
+#define MAX_NUM (100)
+#define FACTOR (10000000000000000)
+static long long arr1[MAX_NUM + 1][MAX_NUM + 1];
+static long long arr2[MAX_NUM + 1][MAX_NUM + 1]; 
 
 int main(void)
 {
-	int n, m;
+	int 		n;
+	int 		m;
+	int 		i;
+	int 		j;
+	int 		cnt = 15;
+	long long 	tmp;
 
 	scanf("%d %d", &n, &m);
-	put_num(arr1, n);
-	put_num(arr2, m);
-	char *res = comb(arr1, arr2);
-	print_arr(res);
+	for (i = 0; i <= n; ++i)
+	{
+		arr1[i][0] = 1;
+		arr1[i][i] = 1;
+	}
+	for (i = 0; i <= n; ++i)
+	{
+		for (j = 0; j <= n; ++j)
+			arr2[i][j] = 0;
+	}
+	for (i = 1; i <= n; ++i)
+	{
+		for (j = 1; j < i; ++j)
+		{
+			arr1[i][j] = arr1[i - 1][j - 1] + arr1[i - 1][j];
+			arr2[i][j] = arr2[i - 1][j - 1] + arr2[i - 1][j];
+			if (arr1[i][j] >= FACTOR)
+			{
+				arr2[i][j]++;
+				arr1[i][j] -= FACTOR;
+			}
+		}
+	}
+	tmp = arr1[n][m];
+	while (tmp > 9)
+	{
+		tmp /= 10;
+		cnt--;
+	}
+	if (arr2[n][m] == 0)
+		printf("%lld\n", arr1[n][m]);
+	else
+	{
+		printf("%lld", arr2[n][m]);
+		while (cnt--)
+			printf("0");
+		printf("%lld", arr1[n][m]);
+	}
 	return (0);
 }
