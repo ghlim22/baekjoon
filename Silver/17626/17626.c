54158be6c215959fd3017e868b506b4ade7a6e79
ghlim22 <ghlim22@mail.hongik.ac.kr>
2023-05-04 23:19:14 +0900

    17626 solved

--- Silver/17626/17626.c
+++ Silver/17626/17626.c
@@ -2,44 +2,26 @@
 #include <math.h>
 #include <limits.h>
 #include <memory.h>
-#define MAX_SIZE (50500)
+#define MAX_SIZE (50000)
 #define MIN(x, y) ((x) <= (y) ? (x) : (y))
 
-static unsigned int dp[MAX_SIZE + 1];
+static int dp[MAX_SIZE + 5];
 
 int main(void)
 {
-    int n, limit, min_cost;
+    int n;
 
     scanf("%d", &n);
-
-    memset(dp, 0xFF, sizeof(dp));
-
-    dp[0] = 0;
-    dp[1] = 1;
-    dp[2] = 2;
-    limit = (int)sqrt(n) + 1;
-
-    for (int i = 2; i <= limit; ++i)
-        dp[i * i] = 1;
-    if (dp[n] == 1)
+    for (int i = 1; i <= n; ++i)
     {
-        printf("%d\n", dp[n]);
-        return (0);
-    }
-
-    for (int i = 3; i <= n; ++i)
-    {
-        if (dp[i] == 1)
-            continue;
         dp[i] = dp[i - 1] + 1;
-        limit = i / 2;
-        for (int j = 2; j <= limit; ++j)
+        for (int j = 2; j <= n; ++j)
         {
-            dp[i] = MIN(dp[i], dp[i - j] + dp[j]);
+            if (j * j > i)
+                break;
+            dp[i] = MIN(dp[i], dp[i - j * j] + 1);
         }
     }
-
     printf("%d\n", dp[n]);
     return (0);
 }
