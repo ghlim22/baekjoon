e494a6d976ff883d46a387524b343343d19a05f7
hyun <hyun22@mail.hongik.ac.kr>
2023-08-18 16:06:32 +0900

    15989 solved

--- Silver/15989/main.cpp
+++ Silver/15989/main.cpp
@@ -1,32 +1,37 @@
 #include <iostream>
 #define MAX_CNT (10000)
+#define ONE (0)
+#define TWO (1)
+#define THREE (2)
 
-static unsigned long long dp[MAX_CNT + 1];
+static unsigned int	dp[MAX_CNT + 1][3];
 
 int main(void)
 {
-	int	test_cnt;
+	int	t;
 	int	n;
 
-	dp[1] = 1;
-	dp[2] = 2;
-	dp[3] = 3;
-	dp[4] = 4;
-	for (int i = 5; i <= MAX_CNT; ++i)
+	/* DP INIT */
+	dp[1][ONE] = 1;
+	dp[2][ONE] = 1;
+	dp[2][TWO] = 1;
+	dp[3][ONE] = 1;
+	dp[3][TWO] = 1;
+	dp[3][THREE] = 1;
+
+	for (int i = 4; i <= MAX_CNT; ++i)
 	{
-		dp[i] = dp[i - 1];
-		if ((i - 2) % 2 == 0 || (i - 2) % 3 == 0)
-			dp[i] += dp[i - 2] - dp[i - 3];
-		if ((i - 3) % 2 == 0 || (i - 3) % 3 == 0)
-			dp[i] += dp[i - 3] - dp[i - 4];
-		
+		dp[i][ONE] = dp[i - 1][ONE];
+		dp[i][TWO] = dp[i - 2][ONE] + dp[i - 2][TWO];
+		dp[i][THREE] = dp[i - 3][ONE] + dp[i - 3][TWO] + dp[i - 3][THREE];
 	}
-	std::cin >> test_cnt;
-	while (test_cnt--)
+
+	std::cin >> t;
+	while (t--)
 	{
 		std::cin >> n;
-		std::cout << dp[n] << std::endl;
+		std::cout << dp[n][ONE] + dp[n][TWO] + dp[n][THREE] << std::endl;
 	}
-	
+
 	return (0);
 }
