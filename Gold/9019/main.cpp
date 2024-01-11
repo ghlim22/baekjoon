b0235610c4ad7c9d233bb147630695eb89c8c9e5
Gyuhyun Lim <hyun22@mail.hongik.ac.kr>
2024-01-11 14:39:19 +0900

    9019 solved:

--- Gold/9019/main.cpp
+++ Gold/9019/main.cpp
@@ -1,10 +1,26 @@
+#include <cmath>
+#include <cstring>
 #include <iostream>
+#include <queue>
 
 int get_distance(int n, int m);
 int func_double(int n);
 int func_subtract(int n);
 int func_left(int n);
 int func_right(int n);
+
+void search();
+
+int (*func[4])(int) = {
+    func_double,
+    func_subtract,
+    func_left,
+    func_right
+};
+
+int start;
+int dest;
+int vis[10000];
 
 int main(void)
 {
@@ -13,24 +29,15 @@ int main(void)
     std::cout.tie(NULL);
 
     int t;
-    int start;
-    int dest;
-    int (*func[4])(int) = {
-        func_double,
-        func_subtract,
-        func_left,
-        func_right
-    };
 
-    // std::cin >> t;
+    std::cin >> t;
 
-    // while (t--)
-    // {
-    // }
-
-    std::cin >> start;
-    for (int i = 0; i < 4; ++i)
-        std::cout << func[i](start) << "\n";
+    while (t--)
+    {
+        std::memset(vis, 0, sizeof(vis));
+        std::cin >> start >> dest;
+        search();
+    }
 
     return 0;
 }
@@ -39,6 +46,10 @@ int get_distance(int n, int m)
 {
     int sum = 0;
 
+    for (int i = 1; i <= 1000; i *= 10)
+    {
+        sum += std::abs((n / i) % 10 - (m / i) % 10);
+    }
     return sum;
 }
 
@@ -66,4 +77,52 @@ int func_right(int n)
     int rmd = n % 10; // right-most digit
 
     return n / 10 + rmd * 1000;
+}
+
+void search()
+{
+    std::queue< std::pair<int, std::string> > q;
+
+    q.push(std::make_pair(start, ""));
+    while (!q.empty())
+    {
+        int next;
+        char ch;
+        int n = q.front().first;
+        std::string trace = q.front().second;
+        
+        if (n == dest)
+            break;
+
+        for (int i = 0; i < 4; ++i)
+        {
+            next = func[i](n);
+            if (!vis[next])
+            {
+                switch (i)
+                {
+                case 0:
+                    ch = 'D';
+                    break;
+                case 1:
+                    ch = 'S';
+                    break;
+                case 2:
+                    ch = 'L';
+                    break;
+                case 3:
+                    ch = 'R';
+                    break;
+                default:
+                    break;
+                }
+
+                vis[next] = 1;
+                q.push(std::make_pair(next, trace + ch));
+            } 
+        }
+        q.pop();
+    }
+
+    std::cout << q.front().second << "\n";
 }
