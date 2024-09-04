5b8e6a2bdd4c78a90804697d2aed2b247081f184
임규현 <hyun22@mail.hongik.ac.kr>
2024-09-04 23:08:06 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 52 ms, Memory: 7904 KB -BaekjoonHub

---
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.cc
@@ -0,0 +1,68 @@
+#include <cstddef>
+#include <iostream>
+#include <vector>
+
+#define N (1000000)
+
+std::vector<int> spf(N + 1);
+std::vector<int> primes;
+bool isPrime[N + 1];
+
+void linearSieve()
+{
+	for (int i = 2; i <= N; ++i) {
+		if (spf[i] == 0) {
+			spf[i] = i;
+			primes.push_back(i);
+			isPrime[i] = true;
+		}
+		for (int p : primes) {
+			long comp = (long)p * i;
+			if (comp > N) 
+				break;
+			spf[comp] = p;
+			if (i % p == 0)
+				break;
+		}
+	}
+}
+
+bool solve(int n)
+{
+	for (int p : primes) {
+		if (p > n / 2) {
+			break;
+		}
+		if (isPrime[n - p]) {
+			std::cout << n << " = " << p << " + " << n - p << '\n';
+			return true;
+		}
+	}
+	return false;
+}
+
+int main(void)
+{
+	std::cin.tie(NULL)->sync_with_stdio(false);
+	
+	int n;
+	int a;
+	int b;
+
+	linearSieve();
+
+	while (true) {
+		std::cin >> n;
+
+		if (n == 0) {
+			break;
+		}
+
+		if (!solve(n)) {
+			std::cout << "Goldbach's conjecture is wrong.\n";
+		}	
+	}
+
+
+	return 0;
+}
