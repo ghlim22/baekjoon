8e02e38dd4910b34d59acadd34fe8535d98844a1
임규현 <hyun22@mail.hongik.ac.kr>
2024-09-14 23:08:27 +0900

    [Silver II] Title: 연속합, Time: 4 ms, Memory: 2208 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.cc
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -1,35 +1,29 @@
-#include <algorithm>
 #include <iostream>
-#include <vector>
 
 int main(void)
 {
 	int n;
+	int num;
+	int cache;
 	int ans;
-	std::vector<int> seq;
-	std::vector<int> cache;
 
 	std::cin.tie(NULL)->sync_with_stdio(false);
 
 	std::cin >> n;
-	for (int i = 0; i < n; ++i) {
-		int num;
-		std::cin >> num;
-		seq.push_back(num);
-	}
-
-	cache.push_back(seq[0]);
-	ans = seq[0];
+	std::cin >> cache;
+	ans = cache;
 	for (int i = 1; i < n; ++i) {
-		if (cache[i - 1] < 0)
-			cache.push_back(seq[i]);
+		std::cin >> num;
+		if (cache < 0)
+			cache = num;
 		else
-		 	cache.push_back(cache[i - 1] + seq[i]);
-		ans = std::max(ans, cache.back());
+		 	cache += num;
+
+		if (cache > ans)
+			ans = cache;
 	}
 
 	std::cout << ans;
-	
 
 	return 0;
 }
