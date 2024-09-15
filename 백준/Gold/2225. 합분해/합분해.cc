6154848c5f23c5669fa532ec29c950f9f101d6ee
임규현 <hyun22@mail.hongik.ac.kr>
2024-09-15 19:49:59 +0900

    [Gold V] Title: 합분해, Time: 0 ms, Memory: 2212 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.cc
+++ 백준/Gold/2225. 합분해/합분해.cc
@@ -3,7 +3,7 @@
 
 #define DIVISOR (1000000000)
 
-int cache[201][201];
+int cache[201];
 
 int main(void)
 {
@@ -13,19 +13,20 @@ int main(void)
 	int k;
 
 	std::cin >> n >> k;
+
+	cache[0] = 0;
+	for (int i = 1; i <= k; ++i) {
+		cache[i] = 1;
+	}
 	
-	for (int i = 0; i <= n; ++i)
-		cache[i][1] = 1;
-	for (int i = 1; i <= k; ++i)
-		cache[0][i] = 1;
 	for (int i = 1; i <= n; ++i) {
 		for (int j = 1; j <= k; ++j) {
-			cache[i][j] = cache[i][j - 1] + cache[i - 1][j];
-			cache[i][j] %= DIVISOR;
+			cache[j] = cache[j - 1] + cache[j];
+			cache[j] %= DIVISOR;
 		}
 	}
 
-	std::cout << cache[n][k];
-
+	std::cout << cache[k];
+	
 	return 0;
 }
