1fd711567bec59082dc83bff9940d700863c702a
임규현 <hyun22@mail.hongik.ac.kr>
2024-09-22 23:02:37 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 1124 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#define MAX (8)
+
+int seq[MAX];
+int n;
+int m;
+
+void comb(int cur, int len) {
+  seq[len - 1] = cur;
+  if (len == m) {
+    for (int i = 0; i < len; ++i) {
+      printf("%d ", seq[i]);
+    }
+    printf("\n");
+    return;
+  }
+
+  for (int i = cur + 1; i <= n; ++i) {
+    comb(i, len + 1);
+  }
+}
+
+int main(void) {
+
+  scanf("%d %d", &n, &m);
+
+  for (int i = 1; i <= n; ++i) {
+    comb(i, 1);
+  }
+
+  return 0;
+}
