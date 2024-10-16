f6445cc7e616a80a03d29f55aa1043b0566fabe8
임규현 <hyun22@mail.hongik.ac.kr>
2024-10-16 22:32:44 +0900

    [Gold IV] Title: 두 동전, Time: 28 ms, Memory: 19772 KB -BaekjoonHub

--- 백준/Gold/16197. 두 동전/두 동전.cc
+++ 백준/Gold/16197. 두 동전/두 동전.cc
@@ -9,85 +9,91 @@
 
 typedef std::pair<int, int> pii_t;
 
-struct point {
-  int y;
-  int x;
+struct pos {
+  int y1;
+  int x1;
+  int y2;
+  int x2;
 };
 
-typedef std::pair<point, point> pair_point_t;
-
-int BOARD[MAX][MAX];
+char BOARD[MAX][MAX];
 int WIDTH;
 int HEIGHT;
+pos init;
 const int DY[4] = {0, -1, 1, 0};
 const int DX[4] = {-1, 0, 0, 1};
-
-std::vector<point> COINS;
 
 void input() {
   std::ios::sync_with_stdio(false);
   std::cin.tie(0);
   std::cout.tie(0);
 
+  init.y1 = -1;
+
   std::cin >> HEIGHT >> WIDTH;
   for (int i = 0; i < HEIGHT; ++i) {
     for (int j = 0; j < WIDTH; ++j) {
-      char c;
-      std::cin >> c;
-      if (c == '#')
-        BOARD[i][j] = 1;
-      else if (c == 'o') {
-        point tmp = {.y = i, .x = j};
-        COINS.push_back(tmp);
+      std::cin >> BOARD[i][j];
+      if (BOARD[i][j] == 'o') {
+        if (init.y1 == -1) {
+          init.y1 = i;
+          init.x1 = j;
+        } else {
+          init.y2 = i;
+          init.x2 = j;
+        }
       }
     }
   }
 }
 
-bool is_in_range(const point &p) {
-  if (!(p.y >= 0 && p.y < HEIGHT))
+bool is_in_range(int y, int x) {
+  if (!(y >= 0 && y < HEIGHT))
     return false;
-  if (!(p.x >= 0 && p.x < WIDTH))
+  if (!(x >= 0 && x < WIDTH))
     return false;
   return true;
 }
 
-bool is_wall(const point &p) { return BOARD[p.y][p.x]; }
-
 int bfs() {
-
   int cost = 0;
-  std::queue<pair_point_t> q;
-  q.push(std::make_pair(COINS[0], COINS[1]));
+  std::queue<pos> q;
+  q.push(init);
 
   while (!q.empty() && cost <= 10) {
-    const int size = q.size();
-    for (int i = 0; i < size; ++i) {
-      pair_point_t f = q.front();
-      point one = f.first;
-      point two = f.second;
+    int qsize = q.size();
+    while (qsize--) {
+      pos p = q.front();
       q.pop();
 
-      if (is_in_range(one) ^ is_in_range(two))
+      if (is_in_range(p.y1, p.x1) ^ is_in_range(p.y2, p.x2))
         return cost;
 
-      if (!is_in_range(one) && !is_in_range(two))
+      if (!is_in_range(p.y1, p.x1) && !is_in_range(p.y2, p.x2))
         continue;
 
-      for (int j = 0; j < 4; ++j) {
-        point next_one = {.y = one.y + DY[j], .x = one.x + DX[j]};
-        point next_two = {.y = two.y + DY[j], .x = two.x + DX[j]};
+      for (int i = 0; i < 4; ++i) {
+        pos np = {.y1 = p.y1 + DY[i],
+                  .x1 = p.x1 + DX[i],
+                  .y2 = p.y2 + DY[i],
+                  .x2 = p.x2 + DX[i]};
 
-        if (is_in_range(next_one) && is_wall(next_one))
-          next_one = one;
-        if (is_in_range(next_two) && is_wall(next_two))
-          next_two = two;
-        if (one.x == next_one.x && one.y == next_one.y && two.x == next_two.x &&
-            two.y == next_two.y)
+        if (is_in_range(np.y1, np.x1) &&
+            BOARD[np.y1][np.x1] == '#') /* Wall */ {
+          np.y1 = p.y1;
+          np.x1 = p.x1;
+        }
+
+        if (is_in_range(np.y2, np.x2) &&
+            BOARD[np.y2][np.x2] == '#') /* Wall */ {
+          np.y2 = p.y2;
+          np.x2 = p.x2;
+        }
+
+        if (np.y1 == p.y1 && np.x1 == p.x1 && np.y2 == p.y2 && np.x2 == p.x2)
           continue;
 
-        pair_point_t next = std::make_pair(next_one, next_two);
-        q.push(next);
+        q.push(np);
       }
     }
     cost++;
@@ -96,7 +102,9 @@ int bfs() {
   return -1;
 }
 
-void solve() { std::cout << bfs(); }
+void solve() {
+    std::cout << bfs();
+}
 
 int main() {
   input();
