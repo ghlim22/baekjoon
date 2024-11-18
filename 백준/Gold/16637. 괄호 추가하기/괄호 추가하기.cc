fca478203243f1ee95dc41d4b0120b28b18311fc
임규현 <hyun22@mail.hongik.ac.kr>
2024-11-18 21:10:47 +0900

    [Gold III] Title: 괄호 추가하기, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.cc
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.cc
@@ -1,12 +1,13 @@
 #include <algorithm>
 #include <cassert>
-#include <cctype>
-#include <cstdint>
-#include <cstring>
+#include <climits>
 #include <iostream>
-#include <vector>
 
-int operate(int lhs, int rhs, char op) {
+int N;
+std::string EXPR;
+int ANSWER = INT_MIN;
+
+int cal(int lhs, int rhs, char op) {
   switch (op) {
   case '+':
     return lhs + rhs;
@@ -19,74 +20,29 @@ int operate(int lhs, int rhs, char op) {
   }
 }
 
+void solve(int sum, int cur) {
+  if (cur >= N) {
+    ANSWER = std::max(ANSWER, sum);
+    return;
+  }
+
+  char op = (cur == 0) ? '+' : EXPR[cur - 1];
+
+  if (cur + 2 < N) {
+    int next = cal(EXPR[cur] - '0', EXPR[cur + 2] - '0', EXPR[cur + 1]);
+    solve(cal(sum, next, op), cur + 4);
+  }
+
+  solve(cal(sum, EXPR[cur] - '0', op), cur + 2);
+}
+
 int main(void) {
-  int n;
-  int answer = INT32_MIN;
-  bool priority[9];
-  std::vector<int> operands;
-  std::vector<char> operators;
-  std::string expr;
+  std::cin >> N;
+  std::cin >> EXPR;
 
-  std::cin >> n;
-  std::cin >> expr;
-  for (char c : expr) {
-    if (std::isdigit(c))
-      operands.push_back(c - '0');
-    else
-      operators.push_back(c);
-  }
+  solve(0, 0);
 
-  /*
-  Bruteforce
-  괄호를 선택하는 모든 경우의 수를 살펴본다.
-  괄호가 중첩될 수 없음에 유의한다.
-  괄호로 묶인 부분을 먼저 계산하고, 나머지를 이후에 계산한다.
-  */
-
-  const int OP_CNT = (n - 1) / 2;
-  const int LIMIT = 1 << OP_CNT;
-  for (int i = 0; i < LIMIT; ++i) {
-    bool flag = false;
-    std::memset(priority, 0, sizeof(priority));
-    for (int j = 0; j < OP_CNT && !flag; ++j) {
-      if (i & (1 << j)) {
-        priority[j] = true;
-        if (j > 0 && priority[j - 1])
-          flag = true;
-      }
-    }
-    if (flag)
-      continue;
-
-    std::vector<int> tmp = operands;
-    std::vector<bool> empty(tmp.size(), false);
-    for (int j = 0; j < OP_CNT; ++j) {
-      if (priority[j]) {
-        int lhs_idx = j;
-        int rhs_idx = j + 1;
-        while (rhs_idx < empty.size() && empty[rhs_idx]) {
-          rhs_idx++;
-        }
-        tmp[rhs_idx] = operate(tmp[lhs_idx], tmp[rhs_idx], operators[j]);
-        empty[lhs_idx] = true;
-      }
-    }
-    for (int j = 0; j < OP_CNT; ++j) {
-      if (!priority[j]) {
-        int lhs_idx = j;
-        int rhs_idx = j + 1;
-        while (rhs_idx < empty.size() && empty[rhs_idx]) {
-          rhs_idx++;
-        }
-        tmp[rhs_idx] = operate(tmp[lhs_idx], tmp[rhs_idx], operators[j]);
-        empty[lhs_idx] = true;
-      }
-    }
-
-    answer = std::max(answer, tmp.back());
-  }
-
-  std::cout << answer;
+  std::cout << ANSWER;
 
   return 0;
 }
