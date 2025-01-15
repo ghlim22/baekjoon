492b9c7dcef81fc460ed77a1e16e7267885caf18
임규현 <hyun22@mail.hongik.ac.kr>
2025-01-15 23:36:34 +0900

    [Gold IV] Title: 부분합, Time: 36 ms, Memory: 2836 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -0,0 +1,47 @@
+#include <algorithm>
+#include <climits>
+#include <iostream>
+
+int sequence[100001];
+int sum[100001];
+int N;
+int S;
+
+int solve(void) {
+  if (sum[N] < S) {
+    return 0;
+  }
+
+  if (sequence[1] >= S) {
+    return 1;
+  }
+
+  int i = 0;
+  int j = 0;
+  int ans = INT_MAX;
+  while (j <= N && i <= j) {
+    if (sum[j] - sum[i] >= S) {
+      ans = std::min(ans, j - i);
+      i++;
+    } else {
+      j++;
+    }
+  }
+
+  if (ans == INT_MAX) {
+    return 0;
+  }
+
+  return ans;
+}
+
+int main(void) {
+  std::cin >> N >> S;
+  for (int i = 1; i <= N; ++i) {
+    std::cin >> sequence[i];
+    sum[i] = sum[i - 1] + sequence[i];
+  }
+  std::cout << solve() << std::endl;
+
+  return 0;
+}
