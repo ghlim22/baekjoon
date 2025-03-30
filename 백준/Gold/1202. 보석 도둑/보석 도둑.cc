29ced5c40373e98684dafc51a753348e009c7151
임규현 <gyuhyun.lim@outlook.com>
2025-03-30 14:20:45 +0900

    [Gold II] Title: 보석 도둑, Time: 160 ms, Memory: 8844 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.cc
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -1,29 +1,28 @@
 #include <algorithm>
 #include <iostream>
 #include <queue>
-#include <vector>
+
+#define fastio                                                                 \
+  do {                                                                         \
+    std::ios::sync_with_stdio(false);                                          \
+    std::cin.tie(0);                                                           \
+    std::cout.tie(0);                                                          \
+  } while (0)
 
 struct jewelry {
   int m;
   int v;
-};
+} jewelries[300000];
 
-bool operator<(const jewelry &l, const jewelry &r) { return l.m < r.m; }
-
-class compare {
-public:
-  bool operator()(const jewelry &a, const jewelry &b) const {
-    return a.v < b.v;
-  }
-};
-
-jewelry jewelries[300000];
 int bags[300000];
 
+bool operator<(const jewelry &lhs, const jewelry &rhs) { return lhs.m < rhs.m; }
+
 int main() {
+  fastio;
+
   int n, k;
   std::cin >> n >> k;
-
   for (int i = 0; i < n; ++i) {
     std::cin >> jewelries[i].m >> jewelries[i].v;
   }
@@ -34,18 +33,16 @@ int main() {
   std::sort(jewelries, jewelries + n);
   std::sort(bags, bags + k);
 
-  std::priority_queue<jewelry, std::vector<jewelry>, compare> pq;
   long total = 0;
-  int j = 0;
-  for (int i = 0; i < k; ++i) {
-    while (j < n && jewelries[j].m <= bags[i]) {
-      pq.push(jewelries[j]);
-      j++;
-    }
-    if (pq.empty()) {
-		continue;
-    }
-    total += pq.top().v;
+  std::priority_queue<int> pq;
+  for (int i = 0, j = 0; i < k; ++i) {
+    while (j < n && jewelries[j].m <= bags[i]) /* push jewelries which can be put into the bag i */
+      pq.push(jewelries[j++].v);
+    if (pq.empty())
+      continue;
+    /* all the jewelries in pq can be put into the bag i */
+    /* select the valuable one in pq */
+    total += pq.top();
     pq.pop();
   }
 
