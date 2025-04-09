fddcd0aaf1501fefa6148c5fc6f2b010c059f933
임규현 <gyuhyun.lim@outlook.com>
2025-04-09 18:45:08 +0900

    [Gold II] Title: 버블 소트, Time: 88 ms, Memory: 5808 KB -BaekjoonHub

---
+++ 백준/Gold/1377. 버블 소트/버블 소트.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <utility>
+#include <algorithm>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+#define precision(x) cout << fixed; cout.precision(x)
+#define ll long long
+#define pii pair<int, int>
+
+using namespace std;
+
+signed main() 
+{
+	fastio;
+	int N;
+
+	cin >> N;
+	pair<int, int> arr[N];
+	for (int i = 0; i < N; ++i)
+	{
+		int input;
+		cin >> input;
+		arr[i].first = input;
+		arr[i].second = i;
+	}
+	sort(arr, arr + N);
+	int ans = 0;
+	for (int i = 0; i < N; ++i)
+		ans = max(ans, arr[i].second - i);
+	cout << (ans + 1) << "\n";
+	return 0;
+}
