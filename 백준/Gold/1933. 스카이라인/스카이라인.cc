55aa234706302091e99993badd2082860cd45f6d
임규현 <gyuhyun.lim@outlook.com>
2025-04-14 13:09:55 +0900

    [Gold I] Title: 스카이라인, Time: 56 ms, Memory: 3388 KB -BaekjoonHub

--- 백준/Gold/1933. 스카이라인/스카이라인.cc
+++ 백준/Gold/1933. 스카이라인/스카이라인.cc
@@ -10,6 +10,22 @@
     std::cout.tie(0);                                                          \
   } while (0)
 
+struct coord {
+  coord() : _y(0), _x(0) {}
+  coord(int y, int x) : _y(y), _x(x) {}
+  bool operator==(const coord &other) {
+    return _y == other._y && _x == other._x;
+  }
+  bool operator!=(const coord &other) {
+    return !(_y == other._y && _x == other._x);
+  }
+  bool inRange(int w, int h) {
+    return (_y >= 0 && _y < h && _x >= 0 && _x < w);
+  }
+  int _y;
+  int _x;
+};
+
 struct Building {
   int left;
   int right;
@@ -17,8 +33,12 @@ struct Building {
 };
 
 typedef std::pair<int, int> pii_t;
-typedef std::vector<pii_t> Result; /* <left, height> */
-typedef std::vector<Building> Buildings;
+typedef std::vector<pii_t> Result;
+
+const int d4y[4] = {0, -1, 1, 0};
+const int d4x[4] = {-1, 0, 0, 1};
+const int d8y[8] = {0, 0, -1, -1, -1, 1, 1, 1};
+const int d8x[8] = {-1, 1, -1, 0, 1, -1, 0, 1};
 
 void append(Result &v, const pii_t &p) {
   if (v.empty()) {
@@ -28,10 +48,8 @@ void append(Result &v, const pii_t &p) {
   if (v.back().second == p.second) { /* 높이가 변하지 않는 경우 */
     return;
   }
-  if (v.back().first == p.first) { /* 같은 x위치에 겹치는 경우, 둘 중 더 높이가
-                                      높은 것만 남긴다. */
-    // v.back().second = std::max(v.back().second, p.second);
-    v.back().second = p.second;
+  if (v.back().first == p.first) { /* x 위치가 중첩되는 경우 */
+    v.back().second = p.second;    /* 더 높은 위치로 갱신 */
     return;
   }
   v.push_back(p);
@@ -39,38 +57,44 @@ void append(Result &v, const pii_t &p) {
 
 Result merge(const Result &left, const Result &right) {
   Result merged;
-  int leftHeight = 0;
-  int rightHeight = 0;
   int leftIndex = 0;
   int rightIndex = 0;
+  int leftHeight = 0;
+  int rightHeight = 0;
+  const int LEFT_SIZE = left.size();
+  const int RIGHT_SIZE = right.size();
 
-  while (leftIndex < left.size() && rightIndex < right.size()) {
-    const auto l = left[leftIndex];
-    const auto r = right[rightIndex];
-    if (l.first < r.first) {
-      leftHeight = l.second;
-      auto p = std::make_pair(l.first, std::max(leftHeight, rightHeight));
+  while (leftIndex < LEFT_SIZE && rightIndex < RIGHT_SIZE) {
+    const pii_t &u = left[leftIndex];
+    const pii_t &v = right[rightIndex];
+    if (u.first < v.first) {
+      leftHeight = u.second;
+      int height = std::max(leftHeight, rightHeight);
+      pii_t p = {u.first, height};
       append(merged, p);
       leftIndex++;
     } else {
-      rightHeight = r.second;
-      auto p = std::make_pair(r.first, std::max(leftHeight, rightHeight));
+      rightHeight = v.second;
+      int height = std::max(leftHeight, rightHeight);
+      pii_t p = {v.first, height};
       append(merged, p);
       rightIndex++;
     }
   }
 
-  while (leftIndex < left.size()) {
-    append(merged, left[leftIndex++]);
+  while (leftIndex < LEFT_SIZE) {
+    append(merged, left[leftIndex]);
+    leftIndex++;
   }
-  while (rightIndex < right.size()) {
-    append(merged, right[rightIndex++]);
+  while (rightIndex < RIGHT_SIZE) {
+    append(merged, right[rightIndex]);
+    rightIndex++;
   }
 
   return merged;
 }
 
-Result solve(const Buildings &v, int lo, int hi) {
+Result solve(std::vector<Building> &v, int lo, int hi) {
   if (lo == hi) {
     return {{v[lo].left, v[lo].height}, {v[lo].right, 0}};
   }
@@ -82,25 +106,22 @@ Result solve(const Buildings &v, int lo, int hi) {
   return merge(left, right);
 }
 
-int main() {
+int main(void) {
   fastio;
 
   int n;
   std::cin >> n;
 
   std::vector<Building> v(n);
-  for (int i = 0; i < n; ++i) {
+  for (int i = 0; i < n; ++i)
     std::cin >> v[i].left >> v[i].height >> v[i].right;
-  }
 
   std::sort(v.begin(), v.end(),
             [](const auto &a, const auto &b) { return a.left < b.left; });
 
   auto result = solve(v, 0, n - 1);
-  for (const auto &p : result) {
+  for (const auto &p : result)
     std::cout << p.first << ' ' << p.second << ' ';
-  }
-  std::cout << std::endl;
 
   return 0;
 }
