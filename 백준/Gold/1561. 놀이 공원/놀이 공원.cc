29e3db80a7b0cccaa209d1cc2a5ba3b84da726fc
임규현 <gyuhyun.lim@outlook.com>
2025-04-21 12:27:42 +0900

    [Gold I] Title: 놀이 공원, Time: 0 ms, Memory: 2248 KB -BaekjoonHub

--- 백준/Gold/1561. 놀이 공원/놀이 공원.cc
+++ 백준/Gold/1561. 놀이 공원/놀이 공원.cc
@@ -14,60 +14,51 @@ int main()
 	for (int i = 0; i < m; ++i)
 		std::cin >> ride[i];
 
-	if (n <= m)
-	{
-		std::cout << n;
-		return 0;
-	}
-
-	long lo = 1;
+	long lo = 0;
 	long hi = 60000000000;
-	int answer = 0;
 	while (lo <= hi)
 	{
 		long mid = (lo + hi) / 2;
 
-		long prevSum = m;
-	    long sum = m;
+		long begin = 0;
+		long last = m;
+
 		for (int i = 0; i < m; ++i)
 		{
-			prevSum += (mid - 1) / ride[i];
-			sum += mid / ride[i];
-			if (n <= prevSum)
-				break;
+			last += (mid / ride[i]);
 		}
 
-		if (n <= sum)
+		begin = last;
+		for (int i = 0; i < m; ++i)
 		{
-			if (n <= prevSum)
-			{
-				hi = mid - 1;
-			}
-			else
-			{
-				long count = prevSum;
-				for (int i = 0; i < m; ++i)
-				{
-					if (mid % ride[i] == 0)
-					{
-						count++;
-					}
-					if (count == n)
-					{
-						answer = i + 1;
-						break;
-					}
-				}
-				break;
-			}
+			if (mid % ride[i] == 0)
+				begin--;
 		}
-		else
+
+		if (n <= begin)
+		{
+			hi = mid - 1;
+		}
+		else if (n > last)
 		{
 			lo = mid + 1;
 		}
+		else
+		{
+			for (int i = 0; i < m; ++i)
+			{
+				if (mid % ride[i] == 0)
+				{
+					begin++;
+				}
+				if (begin == n)
+				{
+					std::cout << i + 1;
+					return 0;
+				}
+			}
+		}
 	}
-
-	std::cout << answer;
 
 	return 0;
 }
