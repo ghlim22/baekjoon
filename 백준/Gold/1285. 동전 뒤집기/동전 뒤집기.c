b589512ca73a9556578c4b4a7a9ff79335f79968
임규현 <gyuhyun.lim@outlook.com>
2025-04-22 17:12:44 +0900

    [Gold I] Title: 동전 뒤집기, Time: 344 ms, Memory: 1124 KB -BaekjoonHub

--- 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.c
+++ 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.c
@@ -1,43 +1,48 @@
 #include <stdio.h>
 
-char coins[20][20];
+char arr[20][20];
 
-int main(void) {
-  int n;
+int min(int a, int b)
+{
+	if (a < b)
+		return a;
+	return b;
+}
 
-  scanf("%d", &n);
-  for (int i = 0; i < n; ++i) {
-    for (int j = 0; j < n; ++j) {
-      char c;
-      scanf(" %c", &c);
-      if (c == 'H')
-        coins[i][j] = 1; /* head */
-    }
-  }
+int main(void)
+{
+	int n;
 
-  int lmt = 1 << n;
-  int answer = n * n;
-  for (int i = 0; i < lmt; ++i) {
-	int tails = 0;
-    for (int col = 0; col < n; ++col) {
-      int column_tails = 0;
-      for (int row = 0; row < n; ++row) {
-        if (i & (1 << row)) {
-          if (coins[row][col]) /* flipped head */
-            column_tails++;
-        } else if (!coins[row][col]) {
-          column_tails++;
-        }
-      }
-	  if (column_tails > n / 2) { /* flip the column or don't */
-		column_tails = n - column_tails;
-	  }
-	  tails += column_tails;
-    }
-	answer = (tails < answer) ? tails : answer;
-  }
+	scanf("%d", &n);
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+			scanf(" %c", &arr[i][j]);
+	}
 
-  printf("%d\n", answer);
+	int answer = 400;
+	for (int i = 0; i < (1 << n); ++i)
+	{
+		int sum = 0;
+		for (int x = 0; x < n; ++x)
+		{
+			int tail = 0;
+			for (int y = 0; y < n; ++y)
+			{
+				if (1 & (i >> y))
+				{
+					tail += (arr[y][x] == 'H' ? 1 : 0);
+				} else {
+					tail += (arr[y][x] == 'T' ? 1 : 0);
+				}
+			}
+			sum += min(tail, n - tail);
+		}
+		
+		answer = min(answer, sum);
+	}
 
-  return 0;
+	printf("%d", answer);
+
+	return 0;
 }
