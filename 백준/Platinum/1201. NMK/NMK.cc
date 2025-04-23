7bf0e1b1f6cf0a70ca18dedb1787549045ec4cdf
임규현 <gyuhyun.lim@outlook.com>
2025-04-23 23:23:17 +0900

    [Platinum III] Title: NMK, Time: 0 ms, Memory: 2056 KB -BaekjoonHub

--- 백준/Platinum/1201. NMK/NMK.cc
+++ 백준/Platinum/1201. NMK/NMK.cc
@@ -1,48 +1,48 @@
 #include <algorithm>
-#include <functional>
 #include <iostream>
 #include <vector>
 
-int main() {
-  int n, m, k;
+int main()
+{
+	int n, m, k;
+	std::cin >> n >> m >> k;
 
-  std::cin >> n >> m >> k;
+	if (!(n >= k + m - 1 && n <= m * k))
+	{
+		std::cout << -1;
+		return 0;
+	}
 
-  if (!(k + m - 1 <= n && n <= m * k)) {
-    std::cout << -1 << std::endl;
-    return 0;
-  }
+	std::vector<int> v(n);
+	for (int i = 0; i < n; ++i)
+		v[i] = i + 1;
 
-  std::vector<int> v;
-  for (int i = 1; i <= n; ++i) {
-    v.push_back(i);
-  }
+	std::vector<int> b(m);
+	b[0] = k;
+	if (m > 1)
+	{
+		int q = (n - k) / (m - 1);
+		int r = (n - k) % (m - 1);
+		for (int i = 1; i < m; ++i)
+		{
+			b[i] = q;
+			if (r > 0)
+			{
+				b[i]++;
+				r--;
+			}
+		}
+	}
 
-  std::vector<int> boundary(m);
-  boundary[0] = k;
-  int q = (n - k) / std::max(1, m - 1);
-  int r = (n - k) % std::max(1, m - 1);
-  for (int i = 1; i < m; ++i) {
-    boundary[i] = boundary[i - 1] + q;
-    if (r > 0) {
-      boundary[i]++;
-      r--;
-    }
-  }
+	for (int i = 0, start = 0; i < m; ++i)
+	{
+		int end = start + b[i];
+		std::sort(v.begin() + start, v.begin() + end, std::greater());
+		start = end;
+	}
 
-  std::greater<int> greater;
-  for (int i = 0; i < m; ++i) {
-    if (i == 0) {
-      std::sort(v.begin(), v.begin() + k, greater);
-    } else {
-      std::sort(v.begin() + boundary[i - 1], v.begin() + boundary[i], greater);
-    }
-  }
+	for (int num : v)
+		std::cout << num << ' ';
 
-  for (int i = 0; i < n; ++i) {
-    std::cout << v[i] << ' ';
-  }
-  std::cout << std::endl;
-
-  return 0;
+	return 0;
 }
