bcccf8e62a8e1043658a4a1a7a9dc3a9d2793849
임규현 <gyuhyun.lim@outlook.com>
2025-05-22 16:30:08 +0900

    [Silver II] Title: 이동하기, Time: 232 ms, Memory: 5968 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.cc
+++ 백준/Silver/11048. 이동하기/이동하기.cc
@@ -1,46 +1,25 @@
 #include <algorithm>
 #include <iostream>
-#include <queue>
 
-struct coord
-{
-	int y;
-	int x;
-};
-
-const int dy[3] = { 1, 0, 1 };
-const int dx[3] = { 0, 1, 1 };
-
-int dp[1000][1000];
+int D[1001][1001];
 
 int main()
 {
 	int N, M;
-
 	std::cin >> N >> M;
-	for (int i = 0; i < N; ++i) {
-		for (int j = 0; j < M; ++j)
-			std::cin >> dp[i][j];
-	}
-
-	for (int y = 0; y < N; ++y) {
-		for (int x = 0; x < M; ++x) {
-			int cur = dp[y][x];
-			int max_val = cur;
-			for (int i = 0; i < 3; ++i) {
-				int py = y - dy[i];
-				int px = x - dx[i];
-				if (!(py >= 0 && py < N && px >= 0 && px < M)) {
-					continue;
-				}
-				int val = cur + dp[py][px];
-				max_val = std::max(max_val, val);
-			}
-			dp[y][x] = max_val;
+	for (int i = 1; i <= N; ++i) {
+		for (int j = 1; j <= M; ++j) {
+			std::cin >> D[i][j];
 		}
 	}
 
-	std::cout << dp[N - 1][M - 1];
+	for (int i = 1; i <= N; ++i) {
+		for (int j = 1; j <= M; ++j) {
+			D[i][j] = std::max(D[i - 1][j], D[i][j - 1]) + D[i][j];
+		}
+	}
+
+	std::cout << D[N][M];
 
 	return 0;
 }
