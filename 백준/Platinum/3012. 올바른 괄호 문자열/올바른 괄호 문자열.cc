fcf0c05b19fd10fafc639d8e1f99fecc83dc0b88
임규현 <gyuhyun.lim@outlook.com>
2025-06-06 00:54:19 +0900

    [Platinum II] Title: 올바른 괄호 문자열, Time: 4 ms, Memory: 2372 KB -BaekjoonHub

--- 백준/Platinum/3012. 올바른 괄호 문자열/올바른 괄호 문자열.cc
+++ 백준/Platinum/3012. 올바른 괄호 문자열/올바른 괄호 문자열.cc
@@ -18,11 +18,12 @@ char close[3] = {
 
 int M = 100000;
 
-long long f(const std::string& s, int i, int j) 
+long long f(const std::string& s, int i, int j)
 {
 	if (i > j) {
 		return 1;
 	}
+
 	if (D[i][j] != -1) {
 		return D[i][j];
 	}
@@ -30,14 +31,12 @@ long long f(const std::string& s, int i, int j)
 	D[i][j] = 0;
 	for (int k = i + 1; k <= j; k += 2) {
 		for (int l = 0; l < 3; ++l) {
-			if (!(open[l] == s[i] || '?' == s[i])) {
+			if (s[i] != open[l] && s[i] != '?')
 				continue;
-			}
-			if (!(close[l] == s[k] || '?' == s[k])) {
+			if (s[k] != close[l] && s[k] != '?')
 				continue;
-			}
-			long long tmp = f(s, i + 1, k - 1) * f(s, k + 1, j);
-			D[i][j] += tmp;
+			
+			D[i][j] += f(s, i + 1, k - 1) * f(s, k + 1, j);
 			if (D[i][j] >= M) {
 				D[i][j] = M + D[i][j] % M;
 			}
@@ -55,10 +54,11 @@ int main()
 	std::memset(D, -1, sizeof(D));
 	f(S, 0, N - 1);
 	if (D[0][N - 1] >= M) {
-		printf("%05lld", D[0][N - 1] % M);
+		std::printf("%05lld\n", D[0][N - 1] % M);
 	}
 	else {
-		std::cout << D[0][N - 1];
+		std::cout << D[0][N - 1] << '\n';
 	}
+
 	return 0;
 }
