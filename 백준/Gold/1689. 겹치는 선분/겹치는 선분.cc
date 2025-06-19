7c27cb6fe2724be494ae87d6922781aa69553de9
임규현 <gyuhyun.lim@outlook.com>
2025-06-20 01:52:37 +0900

    [Gold IV] Title: 겹치는 선분, Time: 1328 ms, Memory: 26712 KB -BaekjoonHub

---
+++ 백준/Gold/1689. 겹치는 선분/겹치는 선분.cc
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <iostream>
+#include <utility>
+#include <vector>
+
+int main()
+{
+	int n;
+	std::cin >> n;
+
+	std::vector<std::pair<int, int>> v;
+	for (int i = 0; i < n; ++i) {
+		int s, e;
+		std::cin >> s >> e;
+		v.push_back({s, 1});
+		v.push_back({e, -1});
+	}
+
+	std::sort(v.begin(), v.end());
+
+	int maxRoom = 0;
+	int room = 0;
+	for (const auto &p : v) {
+		if (p.second == -1) {
+			room--;
+		} else {
+			room++;
+		}
+		maxRoom = std::max(maxRoom, room);
+	}
+
+	std::cout << maxRoom << std::endl;
+	
+	return 0;
+}
