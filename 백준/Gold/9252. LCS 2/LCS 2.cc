6e077d715b1e7def24817b2d4f71de4d18078f30
임규현 <gyuhyun.lim@outlook.com>
2025-09-22 10:57:59 +0900

    [Gold IV] Title: LCS 2, Time: 4 ms, Memory: 6000 KB -BaekjoonHub

---
+++ 백준/Gold/9252. LCS 2/LCS 2.cc
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <iostream>
+#include <stack>
+
+int len[1001][1001];
+std::string a, b;
+int m, n;
+
+std::string backtrack(int i, int j) {
+	if (i == 0 || j == 0) {
+		return "";
+	}
+
+	if (a[i] == b[j]) {
+		return backtrack(i - 1, j - 1) + a[i];
+	} else if (len[i - 1][j] > len[i][j - 1]) {
+		return backtrack(i - 1, j);
+	} else {
+		return backtrack(i, j - 1);
+	}
+}
+
+int main() {
+	std::cin >> a >> b;
+	m = a.size();
+	n = b.size();
+	a = ' ' + a; // a <- ' ' + a[0] + a[1] + a[2] + ... + a[m - 1]
+	b = ' ' + b; 
+	for (int i = 1; i <= m; ++i) {
+		for (int j = 1; j <= n; ++j) {
+			if (a[i] == b[j]) {
+				len[i][j] = len[i - 1][j - 1] + 1;
+			} else {
+				len[i][j] = std::max(len[i - 1][j], len[i][j - 1]);
+			}
+		}
+	}
+
+	std::cout << len[m][n] << '\n';
+	std::cout << backtrack(m, n) << '\n';
+
+	return 0;
+}
