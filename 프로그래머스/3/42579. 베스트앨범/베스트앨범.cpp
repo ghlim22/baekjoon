f2e3f9195a3d4134bc4740f3d3e184f7a451e45a
임규현 <gyuhyun.lim@outlook.com>
2025-10-07 17:51:51 +0900

    [level 3] Title: 베스트앨범, Time: 0.09 ms, Memory: 3.66 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
@@ -1,43 +1,42 @@
-#include <algorithm>
+#include <functional>
+#include <queue>
 #include <string>
-#include <vector>
-#include <map>
-#include <utility>
+#include <unordered_map>
 
 using namespace std;
 
+typedef pair<string, int> psi_t;
+
 vector<int> solution(vector<string> genres, vector<int> plays) {
-    map<string, int> genre_plays;
-    map<string, vector<pair<int, int>>> songs;
+	unordered_map<string, int> genre_plays;
+	for (int i = 0; i < genres.size(); ++i) {
+		genre_plays[genres[i]] += plays[i];
+	}
     
-    for (int i = 0; i < genres.size(); ++i) {
-        const auto &genre = genres[i];
-        const auto &song = std::make_pair(i, plays[i]);
-        songs[genre].push_back(song);
-        genre_plays[genre] += plays[i];
-    }
-    
-    vector<pair<string, int>> genre_plays_vec(genre_plays.begin(), genre_plays.end());
-    sort(genre_plays_vec.begin(), genre_plays_vec.end(), [](const auto &a, const auto &b) {
-        return a.second > b.second;
-    });
-    
-    for (auto &val : songs) {
-        auto &vec = val.second;
-        sort(vec.begin(), vec.end(), [](const auto &a, const auto &b) {
-            return (a.second == b.second) ? (a.first < b.first) : (a.second > b.second);
-        });
-    }
-    
-    vector<int> answer;
-    for (const auto &val1 : genre_plays_vec) {
-        const string &genre = val1.first;
-        const auto &vec = songs[genre];
-        for (int i = 0; i < vec.size(); ++i) {
-            if (i >= 2) break;
-            answer.push_back(vec[i].first);
-        }
-    }
-    
-    return answer;
+	priority_queue<psi_t, vector<psi_t>, function<bool(psi_t, psi_t)>> pq(
+		[&genre_plays, &plays](const psi_t &p1, const psi_t &p2)->bool {
+			if (p1.first != p2.first) 
+				return genre_plays[p1.first] < genre_plays[p2.first];
+			if (plays[p1.second] != plays[p2.second]) 
+				return plays[p1.second] < plays[p2.second];
+			return p1.second > p2.second;
+		}
+	);
+
+	for (int i = 0; i < genres.size(); ++i) {
+		pq.push({genres[i], i});
+	}
+
+	vector<int> answer;
+	unordered_map<string, int> appeared;
+	while (!pq.empty()) {
+		const auto curr = pq.top();
+		pq.pop();
+		if (appeared[curr.first] >= 2) 
+			continue;
+		appeared[curr.first] += 1;
+		answer.push_back(curr.second);
+	}
+
+	return answer;
 }
