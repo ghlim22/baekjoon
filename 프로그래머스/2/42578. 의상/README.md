59b60fa99bcd219dc96085f5592b22c67488a2a3
임규현 <gyuhyun.lim@outlook.com>
2025-10-07 16:33:46 +0900

    [level 2] Title: 의상, Time: 0.03 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578#qna) 
 
 ### 성능 요약
 
-메모리: 4.22 MB, 시간: 8596.56 ms
+메모리: 3.68 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 07일 16:29:20
+2025년 10월 07일 16:33:44
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.cpp
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -4,28 +4,16 @@
 
 using namespace std;
 
-int choose(int n, int k, int i, int sum, vector<int> &v) {
-    if (k == 0) return sum;
-    int ret = 0;
-    for (int j = i; j <= n - k; ++j) {
-        ret += choose(n, k - 1, j + 1, sum * v[j], v);
-    }
-    return ret;
-}
-
 int solution(vector<vector<string>> clothes) {
     map<string, int> m;
-    for (const auto &c : clothes) {
-        m[c[1]]++;
+    for (const auto &cloth : clothes) {
+        m[cloth[1]] += 1;
     }
-    vector<int> v;
-    for (const auto &value : m) {
-        v.push_back(value.second);
+    
+    int answer = 1;
+    for (const auto val : m) {
+        answer *= (val.second + 1);
     }
-    int ret = 0;
-    const int N = v.size();
-    for (int k = 1; k <= N; ++k) {
-        ret += choose(N, k, 0, 1, v);
-    }
-    return ret;
+    
+    return answer - 1;
 }
