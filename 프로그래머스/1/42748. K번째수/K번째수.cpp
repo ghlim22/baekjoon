a2ddb9b0565951b09e5e049ba03cbd60d8d4e17c
임규현 <gyuhyun.lim@outlook.com>
2025-10-19 20:10:14 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.cpp
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -9,15 +9,10 @@ vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
     
     for (const auto &command : commands) {
-        int i = command[0];
-        int j = command[1];
-        int k = command[2];
-        vector<int> v;
-        for (int x = i - 1; x <= j - 1; ++x) {
-            v.push_back(array[x]);
-        }
-        sort(v.begin(), v.end());
-        answer.push_back(v[k - 1]);
+        int i = command[0] - 1, j = command[1] - 1, k = command[2] - 1;
+        auto v = array;
+        sort(v.begin() + i, v.begin() + j + 1);
+        answer.push_back(v[i + k]);
     }
     
     return answer;
