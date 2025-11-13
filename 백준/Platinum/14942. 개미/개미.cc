2cc68bd0c9b9ce2a7827a9c702f50aeb529ae67e
임규현 <gyuhyun.lim@outlook.com>
2025-11-13 21:00:15 +0900

    [Platinum V] Title: 개미, Time: 92 ms, Memory: 38236 KB -BaekjoonHub

--- 백준/Platinum/14942. 개미/개미.cc
+++ 백준/Platinum/14942. 개미/개미.cc
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cmath>
 #include <iostream>
 #include <stack>
 #include <utility>
@@ -11,43 +12,60 @@
 	std::cout.tie(0);                                                          \
   } while (0)
 
-typedef std::pair<int, int> pii_t;
+typedef std::pair<int, long> pii_t;
 
 std::vector<int> energy;
 std::vector<std::vector<pii_t>> graph;
-std::vector<int> answer;
-std::vector<std::vector<pii_t>> reachable;
+pii_t t[18][100001];
+int log2_limit;
+int n;
 
 void dfs(int prev, int current) {
-	answer[current] = current;
-	reachable[current].push_back({current, energy[current]});
-	for (auto [child, distance] : graph[current]) {
-		if (child == prev) {
-			continue;
+	for (const auto &[adj, distance] : graph[current]) {
+		if (adj != prev) {
+			t[0][adj].first = current;
+			t[0][adj].second = distance;
+			dfs(current, adj);
 		}
-		dfs(current, child);
-		for (auto [descendant, remainder] : reachable[child]) {
-			if (remainder == distance) {
-				answer[descendant] = current;
-			} else if (remainder > distance) {
-				answer[descendant] = current;
-				reachable[current].push_back({descendant, remainder - distance});
+	}
+}
+
+void buildTable() {
+	t[0][1].first = 1;
+	t[0][1].second = 0;
+	for (int k = 1; k <= log2_limit; ++k) {
+		for (int i = 1; i <= n; ++i) {
+			const auto &[midNode, midCost] = t[k - 1][i];
+			int to = t[k-1][midNode].first;
+			int distance = midCost + t[k-1][midNode].second;
+			t[k][i].first = to; // i에서 2^(i-1) 이동 -> j + j에서 2^(i-1)번 이동 ==> i에서 2^i번 이동
+			t[k][i].second = distance;
+		}
+	}
+}
+
+void solve() {
+	for (int i = 1; i <= n; ++i) {
+		int node = i;
+		for (int k = log2_limit; k >= 0 && node != 1 && energy[i] > 0; --k) {
+			const auto &[next, distance] = t[k][node];
+			if (distance <= energy[i]) {
+				energy[i] -= distance;
+				node = next;
 			}
 		}
+		std::cout << node << '\n';
 	}
 }
 
 int main(void) {
   fastio;
 
-  int n;
   std::cin >> n;
 
+  log2_limit = std::ceil(std::log2(n));
   energy = std::vector<int>(n + 1);
   graph = std::vector<std::vector<pii_t>>(n + 1);
-  answer = std::vector<int>(n + 1);
-  reachable = std::vector<std::vector<pii_t>>(n + 1);
-
   for (int i = 1; i <= n; ++i) {
 	std::cin >> energy[i];
   }
@@ -60,9 +78,8 @@ int main(void) {
   }
 
   dfs(0, 1);
-  for (int i = 1; i <= n; ++i) {
-	std::cout << answer[i] << '\n';
-  }
+  buildTable();
+  solve();
 
   return 0;
 }
